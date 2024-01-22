c87299c1d2279b58211422f2d77eed5e4181f10d
philemon <philemonabakahmensah@gmail.com>
2024-01-22 00:57:05 +0000

    feat: added json-server which act as an API endpoint

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,16 +1,33 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+     <h1>Job Details Page</h1>
+    <div v-if="job">
+        <h4>{{ job?.title }}</h4>
+        <p>{{ job.description }}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details , please wait ...</p>
+    </div>
 </template>
 
 <script>
 export default {
-    props: ['id']
+    props: ['id'],
     // data(){
     //     return {
     //         id: this.$route.params.id,
     //     }
     // }
+    data(){
+        return{
+            job: null
+        }
+    },
+    mounted(){
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res=> res.json())
+      .then(data => this.job = data)
+      .catch(err=> console.log(err.message))
+    }
 }
 </script>
 
--- src/views/Jobs/JobsView.vue
+++ src/views/Jobs/JobsView.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
     <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
       <h2>{{ job?.title }}</h2> 
     </router-link>
   </div>
+  </div>
+  <div v-else>
+    <p>loading ...</p>
+  </div>
+  
 </template>
 
 <script>
@@ -12,11 +18,17 @@ export default {
     data(){
         return {
             jobs: [
-                {title: "Lemon Developer", id:1, details: "details"},
-                {title: "Lemon Designer", id:2, details: "details"},
-                {title: "Lemon Teaching Assistant", id:3, details: "details"},
+                // {title: "Lemon Developer", id:1, details: "details"},
+                // {title: "Lemon Designer", id:2, details: "details"},
+                // {title: "Lemon Teaching Assistant", id:3, details: "details"},
             ]
         }
+    },
+    mounted(){
+      fetch('http://localhost:3000/jobs')
+      .then(res=> res.json())
+      .then(data => this.jobs = data)
+      .catch(err=> console.log(err.message))
     }
 }
 </script>
